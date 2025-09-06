4915e42f52893a05126b48ef168eb54497d0394b
MINJI <always0524@naver.com>
2025-09-06 21:39:10 +0900

    init. 라우터 설정 파일 생성

---
+++ week4/src/routes/Router.tsx
@@ -0,0 +1,14 @@
+import { useRoutes } from 'react-router-dom'
+import DashBoard from '@/pages/dashboard'
+import Notice from '@/pages/notice'
+import SingIn from '@/pages/sign-in'
+import SignUP from '@/pages/sign-up'
+
+export default function Router() {
+  return useRoutes([
+    { path: '/', element: <DashBoard /> },
+    { path: '/signup', element: <SignUP /> },
+    { path: '/signin', element: <SingIn /> },
+    { path: '/notice', element: <Notice /> },
+  ])
+}
