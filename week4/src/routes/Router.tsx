7e4925c2273c8c9278c85eff2dd155fd5f001229
MINJI <always0524@naver.com>
2025-09-07 11:46:36 +0900

    add. 라우터 설정 파일 프로필 메뉴 path 추가

--- week4/src/routes/Router.tsx
+++ week4/src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { useRoutes } from 'react-router-dom'
 import DashBoard from '@/pages/dashboard'
 import Notice from '@/pages/notice'
+import Profile from '@/pages/profile'
 import SingIn from '@/pages/sign-in'
 import SignUP from '@/pages/sign-up'
 
@@ -10,5 +11,6 @@ export default function Router() {
     { path: '/signup', element: <SignUP /> },
     { path: '/signin', element: <SingIn /> },
     { path: '/notice', element: <Notice /> },
+    { path: '/profile', element: <Profile /> },
   ])
 }
