93edc7174ff81fcc0deabb9a2831169f952f22df
MINJI <always0524@naver.com>
2025-09-07 11:48:54 +0900

    refactor. main 파일 미사용 코드 정리

--- week4/src/main.tsx
+++ week4/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'sonner'
-import { ErrorBoundary } from '@/components'
 import '@/styles/main.css'
 import App from './app'
+import ErrorBoundary from './components/error-boundary'
 
 const root = document.getElementById('root')
 if (!root) throw new Error('문서에 #root 요소가 존재하지 않습니다.')
@@ -11,7 +11,7 @@ if (!root) throw new Error('문서에 #root 요소가 존재하지 않습니다.
 createRoot(root).render(
   <StrictMode>
     <ErrorBoundary>
-      <Toaster position="bottom-center" />
+      <Toaster position="top-center" />
       <App />
     </ErrorBoundary>
   </StrictMode>
