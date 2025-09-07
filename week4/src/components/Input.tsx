666a88551013b70af786e252a7b85441b862cfb9
MINJI <always0524@naver.com>
2025-09-07 11:49:55 +0900

    feat. 인풋 컴포넌트 부모요소 태그 수정 - <></> -> <div></div>

--- week4/src/components/Input.tsx
+++ week4/src/components/Input.tsx
@@ -6,9 +6,9 @@ type InputProps = {
 
 export default function Input({ label, ...props }: InputProps) {
   return (
-    <div>
+    <>
       <label className="block font-medium mb-1">{label}</label>
       <input {...props} required className="w-full border rounded px-3 py-2" />
-    </div>
+    </>
   )
 }
